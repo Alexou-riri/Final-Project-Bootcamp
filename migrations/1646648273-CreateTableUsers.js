084cbfef954bf1147d357c6f65344da4b45e3818
Alex <alexandra.rieux@gmx.com>
2022-03-08 14:12:49 +0100

    Fixed database bug + change database

--- migrations/1646648273-CreateTableUsers.js
+++ migrations/1646648273-CreateTableUsers.js
@@ -3,7 +3,8 @@ exports.up = async (sql) => {
 		CREATE TABLE users (
 			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 			username varchar (40) NOT NULL UNIQUE,
-			password_hash varchar (80) NOT NULL
+			password_hash varchar (80) NOT NULL,
+			company varchar (60) NOT NULL
 
 		);
 	`;
